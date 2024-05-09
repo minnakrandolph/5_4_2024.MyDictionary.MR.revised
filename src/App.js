1e5e1775533723b06b4582bfafa3e23fdef5ed07
minna <minnakrandolph@gmail.com>
2024-05-09 07:44:18 -0400

    Created button to test Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-
+//import logo from "./logo.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> 
-      
+     
       <h1>hello</h1> 
       </header>
-      
+      <a href="#" className="btn btn-primary shadow">button</a>
        
     
     </div>
