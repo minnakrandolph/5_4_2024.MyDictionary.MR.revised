120aa52f6d792a7b08e17d5e962f7ac1396adfe6
minna <minnakrandolph@gmail.com>
2024-05-17 06:45:40 -0400

    Created Dictionary.js w/CSS
    added the foundation of the search engine bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import passionflower from "./passionflower.jpg";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header"> 
-    <img src={passionflower} alt="logo" className="App-logo img-fluid"/>
-      <h1>hello</h1> 
-      
-    
+   <img src={passionflower} alt="logo" className="App-logo img-fluid"/>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="footer text-center"><small>coded by Minna Randolph</small></footer>
+      </div>
       </div>
   );
 }
