930cc17c23c317ea4772cb8ca856a068deb22d68
minna <minnakrandolph@gmail.com>
2024-06-04 07:42:19 -0400

    rendered the meaning
    this should display the definitions in the console

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,22 +2,24 @@ import React from "react";
 
 
 export default function Meaning(props) {
-    console.log(props.meaning);
+    console.log(props.meanings);
     return (
         <div className="Meaning">
-    <h3>{props.meaning.partOfSpeech}</h3>
-    {props.meaning.definitions.map(function(definition, index) {
-  return (
-    <div key={index}>
-        <p>
-        {definition.definition}
-        <br />
-       <em>{definition.example}</em>
-        </p>
-        </div>
-      );
-
-     })}
+    <h3>{props.meanings.partOfSpeech}</h3>
+    {props.meanings.definitions.map(function(definition, index) {
+        return (
+            <div key={index}>
+                
+                <p>
+                {definition.definition}
+                <br />
+               <em>{definition.example}</em>
+                </p>
+                </div>
+        );
+       })}
     </div>
     );
 }
+
+  
