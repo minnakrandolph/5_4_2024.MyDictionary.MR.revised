61a1b6f1fa0dbce234674beec6f70451df885743
minna <minnakrandolph@gmail.com>
2024-06-10 07:14:57 -0400

    created new Synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,20 +1,21 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     console.log(props.meanings);
     return (
-        <div className="Meaning">
+    <div className="Meaning">
     <h3>{props.meaning.partOfSpeech}</h3>
-    <div>
+      <div>
         <p>
-    {props.meaning.definition} 
-      <br />
-       <em>{props.meaning.example}</em>
-                </p>
-                </div>
+        <strong>Definition:</strong> {props.meaning.definition}
+        <br />
+        <strong>Example: </strong>
+        <em>{props.meaning.example}</em>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
+        </p>
+            </div>
         </div>
-        );
+      );
     }
-
-  
---
+++ src/Synonyms.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Synonyms(props) {
+    console.log(props.synonyms)
+    if (props.synonyms) {
+        return (
+            <ul className="Synonyms">
+            {props.meaning.synonyms.map(function (synonyms, index) {
+                return <span key={index}>{synonyms}</span>;
+            })}
+            </ul>
+        );
+} else {
+    return null;
+}
+}
