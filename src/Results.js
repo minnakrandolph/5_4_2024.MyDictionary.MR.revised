d76f1fefb1c46747bf997c36c1d0080af85c8379
minna <minnakrandolph@gmail.com>
2024-06-15 18:31:40 -0400

    created Phonetic.js

--- src/Results.js
+++ src/Results.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
+
 
 export default function Results(props) {
   console.log(props.results);
    if (props.results) {
    return (
    <div className="Results"> 
-   <h2>{props.results.word}
-   </h2>
-   {props.results.meanings.map((meaning, index) => {
+   <h2>{props.results.meaning.phonetic}</h2>
+   {props.results.meaning.phonetic.map((phonetic, index) => {
+    return (
+      <div key={index}>
+        <Phonetic phonetic={phonetic} />
+        </div>
+    );
+   })} 
+   {props.results.meaning.map((meaning, index) => {
     return <div key={index}> 
     <Meaning meaning={meaning} />
     </div>
