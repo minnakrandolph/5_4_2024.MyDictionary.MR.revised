28df4f3c2534fb2fe80b2dbce1a1f4c929fe5161
minna <minnakrandolph@gmail.com>
2024-06-15 18:45:37 -0400

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -9,12 +9,12 @@ export default function Results(props) {
    return (
    <div className="Results"> 
    <h2>{props.results.meaning.phonetic}</h2>
-   {props.results.meaning.phonetic.map((phonetic, index) => {
+   //{props.results.meaning.phonetic.map((phonetic, index) => {
     return (
-      <div key={index}>
-        <Phonetic phonetic={phonetic} />
+     <div key={index}>
+       <Phonetic phonetic={phonetic} />
         </div>
-    );
+   );
    })} 
    {props.results.meaning.map((meaning, index) => {
     return <div key={index}> 
