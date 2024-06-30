26ad57968671102ca0e2a9ed6044c76f5786f2b7
minna <minnakrandolph@gmail.com>
2024-06-30 19:23:54 -0400

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,21 +5,17 @@ import "./Meaning.css";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
+      <div>
      <h3>{props.meaning.partOfSpeech}</h3>
-     {props.meaning.definition.map(function (definition, index)
-     {
-   
-    return (
-      <div key={index}>
-     <div className="definition">{props.meanings.definition}
-    </div>
-        <div className="example">{props.meanings.example}</div>
+     <div className="definition"> 
+      {props.meaning.definition}</div>
+    
+     
+     <div className="example">{props.meaning.example}</div>
       
-        <Synonyms synonyms={props.meanings.synonyms} />
+        <Synonyms synonyms={props.meaning.synonyms} />
       </div>
-          );
-        })}
-        </div>
+      </div>
         );
       }
     
